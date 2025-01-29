ac26fd36655560f22395b1701fa6067e7789c066
DEVRAJ2906 <devrajtadvi2906@gmail.com>
2025-01-29 22:29:59 +0530

    created Printmsg.jsx file and inserted Printmsg function

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Printmsg from './Printmsg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Devraj Tadvi</h1>
+    <>
+      <Printmsg/>
+      <h3>Welcome to the coding world</h3>
+    </>
   )
 }
 
---
+++ 01basicreact/src/Printmsg.jsx
@@ -0,0 +1,7 @@
+function Printmsg() {
+    return(
+        <h1>Hello Devraj</h1>
+    )
+}
+
+export default Printmsg
