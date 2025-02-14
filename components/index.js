45cbaede8937039425e9e066148424c8f75334db
DEVRAJ2906 <devrajtadvi2906@gmail.com>
2025-02-14 19:38:27 +0530

    added all components in one file

--- components/index.js
+++ components/index.js
@@ -1,5 +1,17 @@
 import Card from "./Card";
 import Input from "./Input";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import Contact from "./Contact";
+import User from "./User";
+import Github from "./Github";
 
+export {Github};
+export {User};
+export {Contact};
+export {Home};
+export {Footer};
+export {Header};
 export {Card};
 export {Input};
